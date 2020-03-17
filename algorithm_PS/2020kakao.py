c8c3dd8e96ba176cae0d54a994851062c6f035a5
LeeHyeongKon <ksy37667@gmail.com>
2020-03-17 15:29:55 +0900

    Update 2020kakao.py

--- algorithm_PS/2020kakao.py
+++ algorithm_PS/2020kakao.py
@@ -1,6 +1,5 @@
 def check(p):
     cnt = 0
-    
     for i in p:
         if i == '(':
             cnt += 1
@@ -10,7 +9,10 @@ def check(p):
             cnt -= 1
     return True
 
+
 def recursion(p):
+    if p == '':
+        return p
     left = 0
     right = 0
     u = ''
@@ -25,9 +27,29 @@ def recursion(p):
         if left == right:
             u = p[0:left+right]
             v = p[left+right:]
+            break
+    if check(u) == True:
+        return u + recursion(v)
+    else:
+        tmp = '('
+        tmp += recursion(v) + ')'
+        u = u[1:len(u)-1]
+        for i in range(0,len(u)):
+            if u[i] == '(':
+                tmp += ')'
+            else:
+                tmp += '('
+
+        return tmp
     
 
 def solution(p):
-    if check(p):
+    if check(p) == True:
         return p
     return recursion(p)
+
+
+
+p = '12345'
+
+print(p[0:-1])
