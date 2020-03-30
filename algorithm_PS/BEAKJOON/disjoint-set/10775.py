ba7cb9103554916cd4845b79445c656057a7b0e7
LeeHyeongKon <ksy37667@gmail.com>
2020-03-31 01:25:51 +0900

    Update 10775.py

--- algorithm_PS/BEAKJOON/disjoint-set/10775.py
+++ algorithm_PS/BEAKJOON/disjoint-set/10775.py
@@ -1,36 +1,23 @@
 import sys
 
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(100000)
 
 G = int(input())
 P = int(input())
 gate = {i:i for i in range(G+1)}
 plane = []
-
 for _ in range(P):
     plane.append(int(sys.stdin.readline()))
-
 def find(num):
     if gate[num] == num:
         return num
     gate[num] = find(gate[num])
     return gate[num]
-
-def union(x,y):
-    x = find(x)
-    y = find(y)
-    gate[x] = y
-
-
 cnt = 0
-
 for i in plane:
     tmp = find(i)
     if tmp == 0:
         break
-
-    union(tmp, tmp-1)
-    print(gate)
+    gate[tmp] = tmp - 1
     cnt += 1
-
 print(cnt)
