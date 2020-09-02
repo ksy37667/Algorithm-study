b31c74c0eb945551697e3f1cafe732e259d3fb0e
LeeHyeongKon <ksy37667@gmail.com>
2020-09-02 18:24:39 +0900

    Create 2468.py

---
+++ algorithm_PS/BEAKJOON/BFS/2468.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+def dfs():
+    pass
+
+maxList = []
+M = int(input())
+arr = [0 for _ in range(M)]
+for i in range(M):
+    arr[i] = list(map(int, input().split(' ')))
+    maxList.append(max(arr[i]))
+
+maxValue = max(maxList)
+
+print(maxValue)
