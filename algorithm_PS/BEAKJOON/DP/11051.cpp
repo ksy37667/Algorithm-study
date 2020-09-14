4557a5a2537a138a9e31baa82fa852dbd9072c7d
LeeHyeongKon <ksy37667@gmail.com>
2020-09-14 21:24:10 +0900

    Update 11051.cpp

--- algorithm_PS/BEAKJOON/DP/11051.cpp
+++ algorithm_PS/BEAKJOON/DP/11051.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include <cstring>
-
-using namespace std;
-
-int cache[1000][1000];
-
-int bino(int n, int r){
-	if(r == 0 || n == r) return 1;
-	if(cache[n][r] != -1) return cache[n][r]%10007;
-	return cache[n][r] = bino(n-1,r-1) + bino(n-1,r);
-}
-
-int main(){
-	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int N,R;
-	
-	memset(cache,-1,sizeof(cache));
-	
-	cin >> N >> R;
-	
-	cout << bino(N,R)%10007;
+using namespace std; 
+#define DIV_NUM 10007
+#define NK_MAX 1000 + 1 
+int main() {
+    int N, K; scanf("%d %d", &N, &K);
+    int dp[NK_MAX][NK_MAX] = { 0 };
+    // for 문을 이용하여 n이 1일때부터 갱신합니다.
+    // 각 라운드에서는 k가 0~n 가지일 때를 갱신합니다.
+    for(int n_row = 1; n_row <= N; n_row++){
+        for (int k_col = 0; k_col <= N; k_col++) {
+            if (n_row == k_col || k_col == 0) {
+                dp[n_row][k_col] = 1; continue;
+            }
+            dp[n_row][k_col] = (dp[n_row - 1][k_col] + dp[n_row - 1][k_col - 1]) % DIV_NUM;
+        }
+    }
+    printf("%d", dp[N][K]);
+    return 0;
 }
