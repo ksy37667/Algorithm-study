07ecefbab7bf4d08e870f4c10b927337df1925a3
ksy37667 <ksy37667@gmail.com>
2020-09-25 05:41:42 +0900

    Create LIS.py

---
+++ algorithm_theory/LIS.py
@@ -0,0 +1,36 @@
+def lis(arr):
+    if not arr:
+        return 0
+
+    # C[i] means smallest last number of lis subsequences whose length are i
+    INF = float('inf')
+    C = [INF] * (len(arr)+1)
+    C[0] = -INF
+    C[1] = arr[0]
+    tmp_longest = 1
+
+    # Find i that matches C[i-1] < n <= C[i]
+    def search(lo, hi, n):
+        if lo == hi:
+            return lo
+        elif lo + 1 == hi:
+            return lo if C[lo] >= n else hi
+
+        mid = (lo + hi) // 2
+        if C[mid] == n:
+            return mid
+        elif C[mid] < n:
+            return search(mid+1, hi, n)
+        else:
+            return search(lo, mid, n)
+
+
+    for n in arr:
+        if C[tmp_longest] < n:
+            tmp_longest += 1
+            C[tmp_longest] = n
+        else:
+            next_loc = search(0, tmp_longest, n)
+            C[next_loc] = n
+
+    return tmp_longest
