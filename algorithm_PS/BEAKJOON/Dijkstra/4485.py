4c40df871d93faabfbde46460a8524945d9d31cb
ksy37667 <ksy37667@gmail.com>
2020-09-30 14:10:42 +0900

    Update 4485.py

--- algorithm_PS/BEAKJOON/Dijkstra/4485.py
+++ algorithm_PS/BEAKJOON/Dijkstra/4485.py
@@ -6,39 +6,42 @@ input = sys.stdin.readline
 dd = [(1,0), (-1,0), (0,-1), (0,1)]
 
 def dijkstra(sx,sy):
+    cnt = 0
     heap_data = []
-    heapq.heappush(heap_data, (array[sx][sy], sx, sy))
-    distance[sx][sy] = array[sx][sy]
+    heapq.heappush(heap_data, (cnt, sx, sy))
+    visited[sx][sy] == True
 
     while heap_data:
-        dist, now_x, now_y = heapq.heappop(heap_data)
-        if distance[now_x][now_y] < dist:
-            continue
+        cnt, now_x, now_y = heapq.heappop(heap_data)
+        if now_x == n-1 and now_y == n-1:
+            return cnt
+
         for x, y in dd:
             dx, dy = now_x+x, now_y+y
 
             if dx < 0 or dx >=n or dy < 0 or dy >= n:
                 continue
+            if visited[dx][dy]:
+                continue
+            
+            visited[dx][dy] = True
 
-            cost = dist + array[dx][dy]
-            if distance[dx][dy] > cost:
-                distance[dx][dy] = cost
-                heapq.heappush(heap_data, (cost, dx, dy))
-    return distance[n-1][n-1]
+            if array[dx][dy] == '0':
+                array[dx][dy] = '1'
+                heapq.heappush(heap_data, (cnt + 1, dx, dy))
+            else:
+                heapq.heappush(heap_data, (cnt, dx, dy))
 
-cnt = 1
-while True:
-    n = int(input())
-    if n == 0:
-        break
 
-    array = [[] * n for _ in range(n)]
-    distance = [[INF]*n for i in range(n)]
-    for i in range(n):
-        array[i] =list(map(int, input().split(' ')))
-        
+
+n = int(input())
+
+
+visited = [[False] * n for _ in range(n)]
+
+array = [[] * n for _ in range(n)]
+for i in range(n):
+    array[i] = list(input())[:-1]
     
-    print("Problem",cnt, end="")
-    print(':', dijkstra(0,0))
 
-    cnt += 1
+print(dijkstra(0,0))
